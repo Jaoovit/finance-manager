b63569b8e81145532fc91225671c7152136647b2
João Oliveira <jaoovit@gmail.com>
2024-07-11 19:44:01 +0100

    create the route system

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
+import Create from "./pages/Create"
+import Update from "./pages/Update"
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
   return (
     <div>
-      <Home />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/create" element={<Create />}></Route>
+                <Route path="/update" element={<Update />}></Route>
+            </Routes>
+        </BrowserRouter>
     </div>
   )
 }
